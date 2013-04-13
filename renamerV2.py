ea8b4c8e8de17d965ea080bc73144b770535549f
Steven Shelby <theuberpwnzors@gmail.com>
2013-04-13 01:15:00 -0400

    added renamer v2

---
+++ renamerV2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+__author__ = 'Steven Shelby'
+
+import os,sys
+
+##### CORE FUNCTION #####
+# This should be split out into multiple functions but
+# this was literally a 15 minute project
+def main():
+    cwd = os.getcwd();
+
+    for dirname, dirnames, filenames in os.walk(cwd):
+         # print all filenames.
+        for filename in filenames:
+            print filename
+
+    # get substring from user to remove from file names
+    print ""
+    sub = raw_input("Enter substring to remove from filenames: ")
+    print "Removing \"" + sub + "\" from filenames..."
+    for dirname, dirnames, filenames in os.walk(path):
+        # check each file and rename accordingly
+        for filename in filenames:
+            newfname = filename.replace(sub, "")
+            oldpath = os.path.join(dirname, filename)
+            newpath = os.path.join(dirname, newfname)
+            os.rename(oldpath, newpath)
+
+    # print out new directory listing
+    print ""
+    for dirname, dirnames, filenames in os.walk(path):
+        # print all filenames.
+        for filename in filenames:
+            print filename
+
+##### MAIN #####
+if __name__ == "__main__":
+    main()
